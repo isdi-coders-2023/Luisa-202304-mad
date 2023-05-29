ae5f6ceab7e87cba320c12d8912446c0574a4bca
josealbeldo <rayracing@gmail.com>
2023-05-29 21:59:23 +0200

    Add header test

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -1,35 +1,13 @@
-import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import { Header } from "./Header";
 
-describe("Given Header component", () => {
-  describe("When it is instantiate", () => {
-    // Arrange
-    const label = "Test label";
-    const user = "Test user";
+describe("Header component", () => {
+  test("renders header with logo", () => {
+    render(<Header />);
 
-    beforeEach(() => {
-      // Act
-      render(
-        <Header label={label} user={user}>
-          <p>Menu</p>
-        </Header>
-      );
-    });
+    const logoElement = screen.getByAltText("logo");
 
-    test("Then it should be in the document", () => {
-      const element = screen.getByRole("heading");
-      // Assert
-      expect(element).toBeInTheDocument();
-    });
-    test("it should display props values", () => {
-      // Act
-      const elementLabel = screen.getByText(label);
-      const elementUser = screen.getByText(user);
-      // Assert
-      expect(elementLabel).toBeInTheDocument();
-      expect(elementUser).toBeInTheDocument();
-    });
+    expect(logoElement).toBeInTheDocument();
   });
 });
