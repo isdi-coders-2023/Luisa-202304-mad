6b180d71eb5b6afa5ed488852ef2927ab7ffce36
Lanza <77981833+Lanzadrd@users.noreply.github.com>
2023-05-29 18:25:03 +0200

    fix files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,24 @@
+import { Menu } from "../menu/menu";
+import { MenuOptions } from "../../types/menu.options";
+import { AppRoutes } from "../app.routes/app.routes";
+
 export function App() {
-  return <></>;
+  const label = "Desde fuera";
+  const options: MenuOptions = [
+    { url: "", label: "Home" },
+    { url: "myCharacters", label: "My Characters" },
+    { url: "characterDetails", label: "Character Details" },
+    { url: "addForm", label: "Add Form" },
+    { url: "editForm", label: "Edit Form" },
+    { url: "Error404", label: "Error 404" },
+  ];
+
+  return (
+    <>
+      <Header></Header>
+      <Menu options={options}></Menu>
+      <AppRoutes></AppRoutes>
+      <Footer></Footer>
+    </>
+  );
 }
