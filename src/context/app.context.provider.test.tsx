0cfc2db516bdd3ef04f979b06f5b8d89d920efdc
Javier González <jgestarriol@gmail.com>
2023-05-31 17:47:44 +0200

    Add context test

---
+++ src/context/app.context.provider.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { AppContext, ContextStructure } from "./app.context";
+// const mockContext: ContextStructure = {
+//   characterContext: {
+//     character: [
+//       {
+//         url: "localhost/",
+//         id: 1,
+//         name: "wow",
+//         img: "img",
+//       },
+//     ],
+//   },
+// } as unknown as ContextStructure;
+
+const mockContext: ContextStructure = {
+  characterContext: {
+    character: [
+      {
+        url: "localhost/",
+        id: 1,
+        name: "wow",
+        img: "img",
+      },
+    ],
+  },
+} as unknown as ContextStructure;
+describe("AppContextProvider", () => {
+  test("Then it prints patata", async () => {
+    render(
+      <AppContext.Provider value={mockContext}>
+        <p>Patata</p>
+      </AppContext.Provider>
+    );
+
+    const element = await screen.getByText(/Patata/);
+
+    expect(element).toBeInTheDocument();
+  });
+});
