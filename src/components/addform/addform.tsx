13aedb623dba46794b7bb08fbf63e38c872b4752
josealbeldo <rayracing@gmail.com>
2023-05-31 16:41:41 +0200

    Add styles in form

--- src/components/addform/addform.tsx
+++ src/components/addform/addform.tsx
@@ -1,41 +1,73 @@
+import styles from "./addform.module.scss";
+
 export default function Addform() {
   return (
     <>
-      <div className="loaded-route">
-        <h2>Add Characters:</h2>
-      </div>
-      <div>
-        <h3>Character</h3>
-        <form>
-          <label htmlFor="id">ID</label>
-          <input type="text" name="id" id="id" required></input>
+      <div className={styles.form}>
+        <div>
+          <h2>Add Characters</h2>
 
-          <label htmlFor="name">NAME</label>
-          <input type="text" name="name" id="name" required></input>
-
-          <label htmlFor="gender">GENDER</label>
-          <input type="text" name="gender" id="gender" required></input>
-
-          <label htmlFor="specie">SPECIE</label>
-          <input type="text" name="specie" id="specie" required></input>
-
-          <label htmlFor="status">STATUS</label>
-          <input type="text" name="status" id="status" required></input>
-        </form>
-        <h3>first episode</h3>
-        <form>
-          <label htmlFor="date">DATE</label>
-          <input type="text" name="date" id="date" required></input>
-
-          <label htmlFor="title">TITLE</label>
-          <input type="text" name="title" id="title" required></input>
-        </form>
-
-        <h3>IMAGE URL</h3>
-        <form>
-          <label htmlFor="name"></label>
-          <input type="text" name="name" id="name" required></input>
-        </form>
+          <form className="form-display">
+            <div className="form-container">
+              <div className="box">
+                <h3>First episode</h3>
+                <div>
+                  <label htmlFor="date">DATE</label>
+                  <input type="text" name="date" id="date" required></input>
+                </div>
+                <div>
+                  <label htmlFor="title">TITLE</label>
+                  <input type="text" name="title" id="title" required></input>
+                </div>
+                <div>
+                  <label htmlFor="name">IMAGE URL</label>
+                  <input type="text" name="name" id="name" required></input>
+                </div>
+              </div>
+              <div>
+                <div className="box">
+                  <h3 className="character">Character</h3>
+                  <div>
+                    <label htmlFor="id">ID</label>
+                    <input type="text" name="id" id="id" required></input>
+                  </div>
+                  <div>
+                    <label htmlFor="name">NAME</label>
+                    <input type="text" name="name" id="name" required></input>
+                  </div>
+                  <div>
+                    <label htmlFor="gender">GENDER</label>
+                    <input
+                      type="text"
+                      name="gender"
+                      id="gender"
+                      required
+                    ></input>
+                  </div>
+                  <div>
+                    <label htmlFor="specie">SPECIE</label>
+                    <input
+                      type="text"
+                      name="specie"
+                      id="specie"
+                      required
+                    ></input>
+                  </div>
+                  <div>
+                    <label htmlFor="status">STATUS</label>
+                    <input
+                      type="text"
+                      name="status"
+                      id="status"
+                      required
+                    ></input>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button name="button">SAVE</button>
+          </form>
+        </div>
       </div>
     </>
   );
