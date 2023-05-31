e74a9792b37d4884be54eeea91ad507c4b4bbe87
Alfredo <alfredobenitezjuanilla@gmail.com>
2023-05-31 13:29:38 +0200

    Add error 404 with test and css

---
+++ src/components/errorpage/errorpage.test.tsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Error } from "./errorpage";
+describe("Given 404 error Page", () =>
+  describe("When it is instantiate", () => {
+    test("renders a text in the page ", () => {
+      render(<Error></Error>);
+
+      const role = screen.getByRole("heading");
+
+      expect(role).toBeInTheDocument();
+    });
+  }));
--- src/components/errorpage/errorpage.tsx
+++ src/components/errorpage/errorpage.tsx
@@ -1,7 +1,22 @@
-export default function Error() {
+import styles from "./errorpage.module.scss";
+
+export function Error() {
   return (
-    <div className="loaded-route">
-      <h2>Error 404 hdp</h2>
-    </div>
+    <>
+      <div className="loaded-route">
+        <div className={styles.message}>
+          <h3>THERE S NOTHING HERE</h3>
+          <p>Searching is pain ,you better GOBACK and try again</p>
+          <img
+            src="../../public/portal.png"
+            alt="Rick and Morty go outside the portal"
+            width="70%"
+          ></img>
+          <div className={styles.error404}>
+            <p>404</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
