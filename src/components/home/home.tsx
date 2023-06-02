cee4fe1865eabf36b9ecdb93c0324f1121829768
Javier González <jgestarriol@gmail.com>
2023-06-02 17:52:42 +0200

    fix keys at home

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -14,24 +14,22 @@ export default function Home() {
       <ul className={styles.app}>
         {character.map((item) => {
           return (
-            <>
-              <Link key={item.id} to={"/CharacterDetails/" + item.id}>
-                <div className={styles.characterCard}>
-                  <div className={styles.cardImage}>
-                    <img
-                      className={styles.image}
-                      src={item.image}
-                      alt={styles.characterPortrait}
-                      width={80}
-                    />
-                  </div>
-                  <div>
-                    <p className={styles.id}>#{item.id}</p>
-                    <p className={styles.name}>{item.name}</p>
-                  </div>
+            <Link key={item.id} to={"/CharacterDetails/" + item.id}>
+              <div className={styles.characterCard}>
+                <div className={styles.cardImage}>
+                  <img
+                    className={styles.image}
+                    src={item.image}
+                    alt={styles.characterPortrait}
+                    width={80}
+                  />
                 </div>
-              </Link>
-            </>
+                <div>
+                  <p className={styles.id}>#{item.id}</p>
+                  <p className={styles.name}>{item.name}</p>
+                </div>
+              </div>
+            </Link>
           );
         })}
       </ul>
