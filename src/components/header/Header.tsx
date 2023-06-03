82f26b28c7f4b7f567324f489709b21688f47a9b
Lanza <77981833+Lanzadrd@users.noreply.github.com>
2023-06-03 18:38:54 +0200

    Add logo redirect to homepage

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 type HeaderProps = { children: JSX.Element };
 
 export function Header({ children }: HeaderProps) {
+  const navigate = useNavigate();
+
   return (
     <>
       <header className={styles.header}>
-        <img className={styles.image} src="../../package.png" alt="logo" />
+        <a onClick={() => navigate("/")}>
+          <img className={styles.image} src="../../package.png" alt="logo" />
+        </a>
         {children}
       </header>
     </>
